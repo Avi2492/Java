61d6782fde5e2050ea6856afcc53f9aa7e933896
AVINASH PANDEY <98386256+Avi2492@users.noreply.github.com>
2022-12-28 12:38:08 +0530

    Add files via upload

---
+++ StringFunction.java
@@ -0,0 +1,11 @@
+import java.io.*;
+
+public class StringFunction {
+    public static void main(String[] args) {
+        String str = "Avinash";
+        System.out.println(str.length());
+        System.out.println(str.charAt(2));
+        System.out.println(str.substring(3));
+        System.out.println(str.substring(4, 5));
+    }
+}
